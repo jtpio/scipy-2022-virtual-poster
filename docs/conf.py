4611c72a73be81b3a92026c6494ca9c10c65c471
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2022-06-27 10:40:12 +0200

    Add ReadTheDocs files

---
+++ docs/conf.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+extensions = [
+    'jupyterlite_sphinx'
+]
+
+jupyterlite_config = "jupyterlite_config.json"
+jupyterlite_dir = "."
+
+master_doc = 'index'
+source_suffix = '.rst'
+
+# General information about the project.
+project = 'scipy-2022-jupyterlite-virtual-poster'
+author = 'Jeremy Tuloup'
+
+exclude_patterns = []
+highlight_language = 'python'
+pygments_style = 'sphinx'
+
+html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css'
+]
